a64dca9f6b8281937dbbc88cbdd22e07c57d3ca6
anvitha boppana <anvithaboppana999@gmail.com>
2026-03-28 02:09:01 +0530

    push

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api' });
+baseURL: process.env.REACT_APP_API_URL || 'https://electrostock-backend-rz0q.onrender.com/api'
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
